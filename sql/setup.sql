1976db081818b681ffd53566aeb51be0b4df8064
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-15 11:31:03 -0700

    adding users table and 3 test values

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+INSERT INTO users(first_name, last_name, email, password_hash) VALUES 
+('test1', 'test1', 'test1@test.com', 'nottest1passwordhash'),
+('test2', 'test2', 'test2@test.com', 'nottest2passwordhash'),
+('test3', 'test3', 'test3@test.com', 'nottest3passwordhash');
